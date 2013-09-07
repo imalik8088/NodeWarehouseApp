4c41c483a0d12a3e1614479a58044038e609de41
Wonka <akhlaq87@gmail.com>
2013-09-07 18:43:16 +0200

    added a mocha test

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 /**
  * Module dependencies.
  */
+require('coffee-script');
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -15,7 +14,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -26,9 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
-app.get('/users', user.list);
+// Routes
+require('./apps/authentication/routes')(app)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
